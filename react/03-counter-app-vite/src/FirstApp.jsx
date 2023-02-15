2b4f6e81635a7e3314839b2b558d55c3313ff805
Liuts213 <angel.espinozatorres213@gmail.com>
2023-02-14 16:47:29 -0800

    Se agrego el proceso

---
+++ react/03-counter-app-vite/src/FirstApp.jsx
@@ -0,0 +1,8 @@
+export const FirstApp = () =>{
+    return (
+        <>
+            <h1> Angel </h1>
+            <p>Soy un subtitulo</p>
+        </>
+    );
+}
--- react/03-counter-app-vite/src/main.jsx
+++ react/03-counter-app-vite/src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import {createRoot} from 'react-dom/client';
+import {App} from './HelloWordApp'
+import {FirstApp} from './FirstApp'
+import './styles.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <FirstApp/>
+    </React.StrictMode>
 )
