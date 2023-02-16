196f6a83a1e815de0ac65de1fef59344b21e183b
Liuts213 <angel.espinozatorres213@gmail.com>
2023-02-15 16:51:53 -0800

    Se guarda el nombre del proceso de la pagina

--- react/03-counter-app-vite/src/FirstApp.jsx
+++ react/03-counter-app-vite/src/FirstApp.jsx
@@ -1,8 +1,21 @@
-export const FirstApp = () =>{
+import PropTypes from 'prop-types' //npm install prop-types
+
+export const FirstApp = ({title,subTitle = 'La tierra explota!'}) =>{
+
     return (
         <>
-            <h1> Angel </h1>
-            <p>Soy un subtitulo</p>
+            <h1> {title}</h1>
+            <p>{subTitle + 1}</p>
         </>
     );
+}
+
+FirstApp.propTypes = {
+ title: PropTypes.string.isRequired,
+ subTitle: PropTypes.number.isRequired
+}
+
+FirstApp.defaultProps = {
+    title:'No Hay titulo',
+    subTitle:"No hay subtitulo"
 }
