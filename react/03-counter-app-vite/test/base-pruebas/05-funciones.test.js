12fdebb494c820b844399d82c969cc7b8d8ca3f9
Liuts213 <angel.espinozatorres213@gmail.com>
2023-02-20 17:12:06 -0800

    Se agrego proceso para validaciones

---
+++ react/03-counter-app-vite/test/base-pruebas/05-funciones.test.js
@@ -0,0 +1,30 @@
+import {getUser,getUsuarioActivo} from '../../src/base-pruebas/05-funciones'
+
+describe('pruebas en 05-template-string',() => {
+    test('getUser debe de retornar un objeto',() => {
+      const testUser = {
+        uid: 'ABC123',
+        username:'El_Papi1502'
+      }
+
+      const user = getUser();
+
+      expect(testUser).toEqual(user)
+
+      console.log(user)
+    });
+
+    test('getUsuarioActivo debe de retornar un objeto',() => {
+      
+      const nombre = "El_Papi1502"
+      const user = getUsuarioActivo(nombre);
+
+      expect(user).toEqual({
+        uid: 'ABC567',
+        username:nombre
+      })
+
+      console.log(user)
+    });
+
+})
