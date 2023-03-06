f5939d125b18fc011ccbe3fec290bcf7f48d2428
AngelCertificatedProfessional <angel.espinozatorres213@gmail.com>
2023-03-05 23:04:54 -0800

    se agregan los archivos de pruebas

--- react/04-gif-expert-app/test/components/GifGrid.test.jsx
+++ react/04-gif-expert-app/test/components/GifGrid.test.jsx
@@ -1,8 +1,18 @@
 import { render,screen} from "@testing-library/react"
 import { GifGrid } from "../../src/components/GifGrid"
+import { useFetchGifs } from "../../src/hooks/useFetchGifs"
+
+jest.mock("../../src/hooks/useFetchGifs")
+
 describe('Pruebas en <GifGrid/>', () => {
     const category = 'One Punch'
     test('debe de mostrar el loading inicialmente',()=>{
+        //con esta funcion obligamos al la funcion de usefetchgifts para que regrese el valor que deseamos en este caso imagenes en vasio y loading en true
+        useFetchGifs.mockReturnValue({
+            images:[],
+            isLoading:true
+        })
+
         render(<GifGrid category = {category}/>);
         expect(screen.getByText('Cargando...'));
         expect(screen.getByText(category));
@@ -10,6 +20,24 @@ describe('Pruebas en <GifGrid/>', () => {
     });
 
     test('debe de mostrar items cuando se cargan las imagenes useGFechItems',() =>{
+
+        const gifs=[{
+            id:'ABC',
+            title:'Saitama',
+            url:"https://localhost/saitama.jpg"
+        },
+        {
+            id:'123',
+            title:'Goku',
+            url:"https://localhost/goku.jpg"
+        }]
+
+        useFetchGifs.mockReturnValue({
+            images:gifs,
+            isLoading:true
+        })
+        render(<GifGrid category = {category}/>);
+        expect(screen.getAllByRole('img').length).toBe(2);
 
     });
 })
