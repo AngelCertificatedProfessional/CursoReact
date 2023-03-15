ebf07352bf85c799cad42218e33b3f0f4861e04c
Liuts213 <angel.espinozatorres213@gmail.com>
2023-03-14 17:05:27 -0700

    Se agrega sentencia de intro

---
+++ reactCeroExperto/05-hook-app/src/08-useReducer/todoReducer.js
@@ -0,0 +1,8 @@
+export const todoReducer = (initialState = [],action) => {
+    switch(action.tipe){
+        case 'ABC':
+            return initialState;
+        default:
+            return initialState;
+    }
+}
