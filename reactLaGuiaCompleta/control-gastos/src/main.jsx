7dffdeff5b97362bbdc7e276fa795385d9fccddc
AngelCertificatedProfessional <angel.espinozatorres213@gmail.com>
2023-04-24 21:45:59 -0700

    Se agrego en el proceso el toolkit y el proyecto de gastos

--- reactLaGuiaCompleta/control-gastos/src/main.jsx
+++ reactLaGuiaCompleta/control-gastos/src/main.jsx
@@ -1,15 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './App.jsx'
 import './index.css'
-import { Provider } from 'react-redux'
-import {store} from './store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* Estructura del redux */}
-    <Provider store={store}> 
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
 )
