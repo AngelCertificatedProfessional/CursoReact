356f2bba0b77e7476ed92b21d6d804f5dfce2dc5
AngelCertificatedProfessional <angel.espinozatorres213@gmail.com>
2023-10-30 22:13:12 -0700

    Se agrego el agregar en el auth js

--- reactAplicacionsocket/06-chat/chat-backend/controllers/auth.js
+++ reactAplicacionsocket/06-chat/chat-backend/controllers/auth.js
@@ -1,10 +1,40 @@
 const {response} = require('express')
+const bcrypt = require('bcryptjs')
+const Usuario = require('../models/usuario')
 
 const crearUsuario = async(req,res = response) => {
-    res.json({
-        ok:true,
-        usuario:'ABC'
-    })
+
+    try{
+        const {email,password} = req.body;
+
+        const existeEmail = await Usuario.findOne({email})
+
+        if(existeEmail){
+            return res.status(400).json({
+                ok:false,
+                msg:'El correo ya existe'
+            })
+        }
+
+        const usuario = new Usuario(req.body)
+        //Encriptar contrasena
+        const salt = bcrypt.genSaltSync()
+        usuario.password = bcrypt.hashSync(password,salt);
+        //Guardar usuario en BD
+        await usuario.save()
+        res.json({
+            ok:true,
+            usuario
+        })
+    }catch(error){
+        console.log(error);
+        res.status(500).json({
+            ok:false,
+            msg: 'hable con el administrador'
+        })
+    }
+
+
 }
 
 const login = async(req,res = response) => {
