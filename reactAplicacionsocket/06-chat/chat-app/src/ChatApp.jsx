23038460a9b702023893fc957eece736680d0789
AngelCertificatedProfessional <angel.espinozatorres213@gmail.com>
2023-11-16 21:03:03 -0800

    Se agrega el proceso de reducer de chatgpt

--- reactAplicacionsocket/06-chat/chat-app/src/ChatApp.jsx
+++ reactAplicacionsocket/06-chat/chat-app/src/ChatApp.jsx
@@ -1,13 +1,16 @@
 import { AuthProvider } from './auth/AuthContext';
 import { SocketProvider } from './context/SocketContext';
+import { ChatProvider } from './context/chat/ChatContext';
 import { AppRouter } from './router/AppRouter';
 
 export const ChatApp = () => {
     return (
-        <AuthProvider>
-            <SocketProvider>
-                <AppRouter />
-            </SocketProvider>
-        </AuthProvider>
+        <ChatProvider>
+            <AuthProvider>
+                <SocketProvider>
+                    <AppRouter />
+                </SocketProvider>
+            </AuthProvider>
+        </ChatProvider>
     )
 }
