0b2c7c16296724880e8ce45283425f91b2a2daf6
AngelCertificatedProfessional <angel.espinozatorres213@gmail.com>
2024-03-19 21:14:30 -0700

    Se agrega comentarios para el entendimiento del codigo

--- reactAplicacionsocket/06-chat/chat-app/src/context/chat/ChatReducer.jsx
+++ reactAplicacionsocket/06-chat/chat-app/src/context/chat/ChatReducer.jsx
@@ -7,28 +7,30 @@ import { types } from "../../types/types";
 //     mensajes:[] //El chat seleccionado
 // }
 
-export const chatReducer = (state,action) => {
-    switch(action.type){
+export const chatReducer = (state, action) => {
+    switch (action.type) {
         case types.usuariosCargados:
             return {
                 ...state,
                 usuarios: [...action.payload]
             }
         case types.activarChat:
-            if(state.chatActivo === action.payload) return state;
+            //evita que se eliminen los mensajes
+            if (state.chatActivo === action.payload) return state;
+            //limpiamos los mensajes
             return {
                 ...state,
                 chatActivo: action.payload,
-                mensajes:[]
+                mensajes: []
             }
         case types.nuevoMensaje:
-            if(state.chatActivo === action.payload.de || 
-                state.chatActivo === action.payload.para){
+            if (state.chatActivo === action.payload.de ||
+                state.chatActivo === action.payload.para) {
                 return {
                     ...state,
-                    mensajes:[...state.mensajes,action.payload]
+                    mensajes: [...state.mensajes, action.payload]
                 }
-            }else{
+            } else {
                 return state;
             }
         case types.cargarMensajes:
@@ -38,10 +40,10 @@ export const chatReducer = (state,action) => {
             }
         case types.cerrarSesion:
             return {
-                uid:'',
-                chatActivo:null,
-                usuarios:[],
-                mensajes:[]
+                uid: '',
+                chatActivo: null,
+                usuarios: [],
+                mensajes: []
             }
         default:
             return state;
