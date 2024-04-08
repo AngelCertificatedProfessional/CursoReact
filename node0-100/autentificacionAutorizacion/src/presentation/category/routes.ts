6e1866917ed2013bf46a302d484bff291a435330
AngelCertificatedProfessional <angel.espinozatorres213@gmail.com>
2024-04-08 00:05:31 -0700

    Se agrea middleware para validar la ruta

--- node0-100/autentificacionAutorizacion/src/presentation/category/routes.ts
+++ node0-100/autentificacionAutorizacion/src/presentation/category/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { CategoryController } from './controller';
+import { AuthMiddleware } from '../middlewares/auth.middleware';
 
 export class CategoryRoutes {
     static get routes(): Router {
@@ -8,7 +9,9 @@ export class CategoryRoutes {
         // Definir las rutas
         // router.use('/api/todos', /*TodoRoutes.routes */ );
         router.get('/', controller.getCategories)
-        router.post('/', controller.createCategory)
+        router.post('/', [
+            AuthMiddleware.validateJWT
+        ], controller.createCategory)
         return router;
     }
 }
