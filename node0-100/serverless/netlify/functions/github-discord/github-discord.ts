87037326f440eaf5111b60586272f0f512bdb8ed
Liuts213 <angel.espinozatorres213@gmail.com>
2024-04-23 16:44:11 -0700

    Se agrega codigo del proyecto de github

--- node0-100/serverless/netlify/functions/github-discord/github-discord.ts
+++ node0-100/serverless/netlify/functions/github-discord/github-discord.ts
@@ -22,8 +22,44 @@ const notify = async (message: string) => {
     return true
 }
 
+
+const onStart = (payload: any): string => {
+    let message: string = '';
+    const { action, sender, repository, starred_at } = payload;
+    return `User ${sender.login} ${action} star on ${repository}`
+}
+
+const onIssue = (payload: any) => {
+    const { action, issue } = payload;
+    if (action === 'opened') {
+        return `An issue was opened with this title ${issue.title}`
+    }
+    if (action === 'closed') {
+        return `An issue was closed by ${issue.user.login}`
+    }
+    if (action === 'reopened') {
+        return `An issue was closed by ${issue.user.login}`
+    }
+    return `Unhandle action for the issue event ${action}`
+
+}
+
 export default async (req: Request, context: Context) => {
-    await notify('hola mundo desde netlify dev')
+
+    const githubEvent = req.headers.get('x-github-event') ?? 'unknown';
+    const payload = req.body;
+    let message: string;
+    switch (githubEvent) {
+        case 'start':
+            message = onStart(payload)
+            break;
+        case 'issues':
+            message = onIssue(payload)
+        default:
+            message = `Unknown event ${githubEvent}`
+    }
+
+    await notify(message)
     return new Response(
         JSON.stringify({ message: 'hola mundo 3' }),
         { status: 401 }
