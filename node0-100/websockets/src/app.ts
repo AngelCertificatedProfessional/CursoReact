b761dd27201211c7632af3bd6dbb0c2d34554d19
Liuts213 <angel.espinozatorres213@gmail.com>
2024-04-25 16:59:07 -0700

    Se agrega profeso del websocket

--- node0-100/websockets/src/app.ts
+++ node0-100/websockets/src/app.ts
@@ -1,4 +1,4 @@
-import { WebSocketServer } from 'ws';
+import { WebSocketServer, WebSocket } from 'ws';
 
 const wss = new WebSocketServer({ port: 8097 });
 
@@ -7,15 +7,28 @@ wss.on('connection', function connection(ws) {
     ws.on('error', console.error);
 
     ws.on('message', function message(data, isBinary) {
-        console.log('received: %s', data);
 
 
-        const payload = {
+
+        const payload = JSON.stringify({
             type: 'custom-payload',
             payload: data.toString()
-        }
+        })
 
-        ws.send(JSON.stringify(payload))
+        // ws.send(JSON.stringify(payload))
+        //todos incluyente
+        // wss.clients.forEach(function each(client) {
+        //     if (client.readyState === WebSocket.OPEN) {
+        //         client.send(payload, { binary: false });
+        //     }
+        // });
+        //todos excluyentes
+        wss.clients.forEach(function each(client) {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(payload, { binary: false });
+            }
+        });
+
     });
 
     // ws.send('hola desde el servidor!');
