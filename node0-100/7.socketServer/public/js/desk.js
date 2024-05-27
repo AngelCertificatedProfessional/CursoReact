052682bb752d81e82e6d4c3a9fdb554cb5a5bce2
Liuts213 <angel.espinozatorres213@gmail.com>
2024-05-27 14:10:13 -0700

    Se agrega proceso de rutas

--- node0-100/7.socketServer/public/js/desk.js
+++ node0-100/7.socketServer/public/js/desk.js
@@ -1,3 +1,37 @@
+//Referencias HTML
+const lblPending = document.querySelector("#lbl-pending")
 
 
-console.log('Escritorio HTML');
+async function loadInitialCount() {
+    const pending = await fetch('/api/ticket/pending').then(resp => resp.json())
+    lblPending.innerHTML = pending.length || 0;
+}
+
+function connectToWebSockets() {
+
+    const socket = new WebSocket('ws://localhost:8120/ws');
+
+    socket.onmessage = (event) => {
+        const { type, payload } = JSON.parse(event.data)
+        if (type !== 'on-ticket-count-changed') return;
+        lblPending.innerHTML = payload
+    };
+
+    socket.onclose = (event) => {
+        console.log('Connection closed');
+        setTimeout(() => {
+            console.log('retrying to connect');
+            connectToWebSockets();
+        }, 1500);
+
+    };
+
+    socket.onopen = (event) => {
+        console.log('Connected');
+    };
+
+}
+
+
+loadInitialCount()
+connectToWebSockets()
--- node0-100/7.socketServer/src/presentation/services/ticket.service.ts
+++ node0-100/7.socketServer/src/presentation/services/ticket.service.ts
@@ -1,8 +1,15 @@
 import { ok } from "assert";
 import { UuidAdapter } from "../../config/uuid.adapter";
 import { Ticket } from "../../domain/interfaces/ticket";
+import { WssService } from "./wss.service";
 
 export class TicketService {
+
+    constructor(
+        private readonly wssService = WssService.instance
+    ) {
+    }
+
     public readonly tickets: Ticket[] = [
         { id: UuidAdapter.v4(), number: 1, createAt: new Date(), done: false },
         { id: UuidAdapter.v4(), number: 2, createAt: new Date(), done: false },
@@ -36,6 +43,7 @@ export class TicketService {
             handleAtDesk: undefined
         }
         this.tickets.push(ticket)
+        this.onTicketNumberChanged();
         return ticket;
     }
 
@@ -63,6 +71,8 @@ export class TicketService {
         return { status: 'ok' }
     }
 
-
+    private onTicketNumberChanged() {
+        this.wssService.sendMessage('on-ticket-count-changed', this.pendingTickets.length);
+    }
 
 }
