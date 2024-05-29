14b8f946723eddd71f5298c92d39fbf6341b9458
Liuts213 <angel.espinozatorres213@gmail.com>
2024-05-28 17:02:31 -0700

    Se agrega proceso de desk para validar que no se pueda ingresar si no tiene un desk configurado

--- node0-100/7.socketServer/public/js/desk.js
+++ node0-100/7.socketServer/public/js/desk.js
@@ -1,5 +1,16 @@
 //Referencias HTML
 const lblPending = document.querySelector("#lbl-pending")
+const deskHeader = document.querySelector("h1")
+
+const searchParams = new URLSearchParams(window.location.search)
+
+if (searchParams.has('escritorio')) {
+    window.location = 'index.html'
+    throw new Error('Escritorio es requerido')
+}
+
+const deskNumber = searchParams.get('escritorio')
+deskNumber.innerText = deskNumber;
 
 
 async function loadInitialCount() {
